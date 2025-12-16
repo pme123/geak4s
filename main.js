d6c6128311bb1ef498b74915b37b841f83b0c56a
Pascal Mengelt <pascal.mengelt@gmail.com>
2025-12-16 17:32:33 +0100

    Adjustments in main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import './src/main/resources/styles.css'
 import 'scalajs:main.js'
-import "@ui5/webcomponents/dist/Assets.js";
-import "@ui5/webcomponents-fiori/dist/Assets.js";
-import "@ui5/webcomponents-icons/dist/Assets.js";
 
 // Import SheetJS
 import * as XLSX from 'xlsx';
